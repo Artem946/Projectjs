733ad4b4081b0a228e809a888742cdce6d5dfd53
Artem <derevenskih@it-thematic.ru>
2020-03-02 15:15:25 +0300

    create first task

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-"use strict"
-// a = 3;
-// console.log(a)
-// let person ={
-//     name: "Тема",
-//     age: 23,
-//     notIsMarried: false
-// };
-// console.log(person.age)
-// let answer = confirm("Are you ready?");
-// console.log(answer);
-// let answer = prompt("Are you ready?", "Yes");
-// console.log(answer);
+// "use strict"
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+
+alert(appData.budget / 30);
