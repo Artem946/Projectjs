19977ad9d77e42503370bccb486eaa5d7be6a148
Artem <derevenskih@it-thematic.ru>
2020-03-02 14:17:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict"
+// a = 3;
+// console.log(a)
+// let person ={
+//     name: "Тема",
+//     age: 23,
+//     notIsMarried: false
+// };
+// console.log(person.age)
+// let answer = confirm("Are you ready?");
+// console.log(answer);
+// let answer = prompt("Are you ready?", "Yes");
+// console.log(answer);
