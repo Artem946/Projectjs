1260dd8b5341b6e9127815ecd94a655f96d25cf2
Artem <derevenskih@it-thematic.ru>
2020-03-05 14:11:14 +0300

    добавил модальные окна и функию расчета накопления

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict"
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    
+    while(isNaN(money)|| money == "" || money == null ){
+
+    }
+}
+start();
 
 let appData = {
 	budget: money,
@@ -8,21 +17,27 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true,
 };
 
 
-	
-for (let i = 0 ;i < 2 ; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	    b = prompt("Во сколько обойдется?", '');
-  if ((typeof(a))==="string"&& (typeof(a)) !=null && (typeof(b)) !=null
-  && a != ''&& b != ''&& a.length < 30) {
-      console.log("done");
-      appData.expenses [a]= b;
-  }
+function choseExpenses() {
+    for (let i = 0 ;i < 2 ; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+      if ((typeof(a))==="string"&& (typeof(a)) !=null && (typeof(b)) !=null
+      && a != ''&& b != ''&& a.length < 30) {
+          console.log("done");
+          appData.expenses [a]= b;
+      }else {
+          i = i -1;
+      }
+    }
 }
-appData.moneyPerDay = appData.budget / 30;
+
+choseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
 alert("Ежедневный бюджет:" +appData.moneyPerDay);
 
 if(appData.moneyPerDay > 100) {
@@ -35,5 +50,14 @@ if(appData.moneyPerDay > 100) {
     console.log("Вешайся")
 }
 
+function checkSavings() {
+    if (appData.savings == true){
+        let save = +prompt ("Какова сумма накоплений?"),
+            percent =+prompt("Под какой процент?");
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход с депозита :" + appData.monthIncome);
+    }
+}
+checkSavings();
 
 
